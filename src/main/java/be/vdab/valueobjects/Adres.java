f629d84b8b1d0e545456096fc500b0b2e0f286ad
olivier <olivier.deleye@gmail.com>
2014-01-13 15:11:29 +0100

    under construction 2

--- src/main/java/be/vdab/valueobjects/Adres.java
+++ src/main/java/be/vdab/valueobjects/Adres.java
@@ -7,5 +7,23 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class Adres implements Serializable{
 
+	private String adres;
+	private int postCode;
+	private String gemeente;
+	
+	
+	
+	
+	
+	
+	public String getAdres() {
+		return adres;
+	}
+	public int getPostCode() {
+		return postCode;
+	}
+	public String getGemeente() {
+		return gemeente;
+	}
 	
 }
